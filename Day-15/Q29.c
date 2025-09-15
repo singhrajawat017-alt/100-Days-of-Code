bab0e5a99cd45a6310dfeb261206db62eed350e3
Rohit Singh Rajawat <singhrajawat017@gmail.com>
2025-09-15 22:02:43 +0530

    Day-15

---
+++ Day-15/Q29.c
@@ -0,0 +1,13 @@
+//Q29. Write a program to calculate the factorial of a number.
+#include<stdio.h>
+int main(){
+    int n,fact;
+    fact = 1;
+    printf("Enter number :");
+    scanf("%d",&n);
+    for(int i = 1; i <= n ; i++){
+        fact *= i;
+    }
+    printf("%d",fact);
+    return 0; 
+}
---
+++ Day-15/Q30.c
@@ -0,0 +1,16 @@
+//Q30. Write a program to reverse a given number.
+#include<stdio.h>
+int main(){
+    int n ,rev,rem;
+    rev = 0;
+    printf("Enter number:");
+    scanf("%d",&n);
+    while (n!=0)
+    {
+        rem = n % 10;
+        rev= rev*10 + rem;
+        n = n/10;        
+      }
+      printf("Reverse number : %d",rev);
+      return 0;
+}
