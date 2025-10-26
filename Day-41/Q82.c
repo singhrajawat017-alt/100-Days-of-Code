8f15cc6544f4bce9b3641fa89ff6cff0a6732b04
Rohit Singh Rajawat <singhrajawat017@gmail.com>
2025-10-26 22:56:52 +0530

    Day-41

---
+++ Day-41/Q82.c
@@ -0,0 +1,13 @@
+//Q82.Print each character of a string on a new line.
+#include <stdio.h>
+int main() {
+    char str[100];
+    int i;
+    printf("Enter a string: ");
+    gets(str); 
+    printf("\nCharacters in the string:\n");
+    for(i = 0; str[i] != '\0'; i++) {
+        printf("%c\n", str[i]);
+    }
+    return 0;
+}
