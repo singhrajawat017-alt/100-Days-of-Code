bb8d6e58c9e4080b085958584a213109952186cd
Rohit Singh Rajawat <singhrajawat017@gmail.com>
2025-12-23 13:42:33 +0530

    Day-58

---
+++ Day-58/Q108.c
@@ -0,0 +1,37 @@
+/*Q108: Write a Program to take an integer array nums. Print an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i]. The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+*/
+#include<stdio.h>
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    int nums[n], answer[n];
+
+    for(int i = 0; i < n; i++){
+        scanf("%d", &nums[i]);
+        answer[i] = 1;
+    }
+
+    int prefix = 1;
+    for ( int i = 0; i < n; i++){
+        answer[i] = prefix;
+        prefix *= nums[i];
+    }
+
+    int suffix = 1;
+    for ( int i = n - 1; i >= 0; i--){
+        answer[i] *= suffix;
+        suffix *= nums[i];
+    }
+    printf("[");
+    for(int i = 0; i < n; i++){
+        printf("%d", answer[i]);
+        if( i != n - 1){
+            printf(",");
+        }
+    }
+    printf("]");
+
+    return 0;
+}
